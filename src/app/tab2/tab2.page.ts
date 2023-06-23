a8cf76a61901d402e7318b08af3c8d583e47250b
andresmdkr <andresmdkr@gmail.com>
2023-06-22 21:20:37 -0300

    Searchbar

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MealService } from '../services/meal.service';
+import { Meal, Response } from '../interfaces/interfaces';
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  searchResults: Meal[] = [];
+  showNoResultsMessage: boolean = false;
 
-  constructor() {}
+  constructor(private mealService: MealService) {
+    this.searchResults = [];
+  }
+  search(event: any) {
+    const searchQuery = event.detail.value;
+    if (searchQuery && searchQuery.trim() !== '') {
+      this.mealService.getMealByName(searchQuery).subscribe((data: Response) => {
+        console.log(data.meals);
+        this.searchResults = data.meals;
+        this.showNoResultsMessage = this.searchResults.length === 0; // Mostrar el mensaje si no se obtienen resultados
+      });
+    } else {
+      this.searchResults = []; // Reiniciar los resultados de búsqueda si la consulta está vacía
+      this.showNoResultsMessage = false; // Ocultar el mensaje cuando se realiza una búsqueda vacía
+    }
+  }
 
+  resetSearch() {
+    this.searchResults = [];
+    this.showNoResultsMessage = false;
+  }
 }
+
