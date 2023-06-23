2145e838884bd3baeb1dbf55afda70700b41eced
andresmdkr <andresmdkr@gmail.com>
2023-06-23 00:51:41 -0300

    Meal-Detail

--- src/app/meal-detail/meal-detail.page.ts
+++ src/app/meal-detail/meal-detail.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+import { Meal, Response } from '../interfaces/interfaces';
+import { MealService } from '../services/meal.service';
+import {LoadingController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-meal-detail',
@@ -8,15 +11,31 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./meal-detail.page.scss'],
 })
 export class MealDetailPage implements OnInit {
+  Meal: Meal[] = [];
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private mealService: MealService,private route: ActivatedRoute,private loadingController: LoadingController,private router: Router) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    const loading = await this.loadingController.create({
+      message:"Cargando comida..."
+    });
+
+    loading.present();
     this.route.paramMap.subscribe(params => {
       const mealId = params.get('id');
-      console.log('ID:', mealId);
-     
+
+      this.mealService.getMealDetail(mealId).subscribe((data: Response) => {
+        console.log(data.meals);
+        this.Meal = data.meals;
+        loading.dismiss();
+      });
+      
     });
+    
   }
+  goToHome(){
+    this.router.navigate(['/tabs/meals']);
+  }
+
 
 }
